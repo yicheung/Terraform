b90420bda1db4527ba4a2219a01a404a4186d5fa
yicheung <yicheung@yahoo.com>
2022-01-07 13:19:27 -0500

    Added local ingress to test tfsec commenter

--- testapp.tf
+++ testapp.tf
@@ -120,6 +120,13 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["192.168.1.0/24"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.2.0/24"]
+  }
+
   # outbound internet access
   egress {
     description = "For user ${var.username}, create by terraform"
