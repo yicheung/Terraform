a775a4ff76c740c7e41aa5c1124c3239e796a8e7
yicheung <yicheung@yahoo.com>
2022-01-07 12:53:42 -0500

    test adding local network on ingress without description

--- testapp.tf
+++ testapp.tf
@@ -112,6 +112,13 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
+  # Local network
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.1.0/24"]
+  }
 
   # outbound internet access
   egress {
