ad09b9f6b6d320aa00fd5727e5c1f73a445146ed
yicheung <yicheung@yahoo.com>
2022-01-13 11:24:22 -0500

    test tf sarif actions

--- testapp.tf
+++ testapp.tf
@@ -285,6 +285,12 @@ resource "aws_autoscaling_group" "autobot" {
     value               = "TEST"
     propagate_at_launch = false
   }
+
+  tag {
+    key                 = "APPLICATION"
+    value               = "tf-test"
+    propagate_at_launch = false
+  }
 }
 
 resource "aws_route53_record" "www" {
